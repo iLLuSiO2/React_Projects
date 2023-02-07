14476cdd26bf2b5378c9f12290293aaca9fbd0dc
Nadar Jilbin Sam Sudar <107865600+iLLuSiO2@users.noreply.github.com>
2023-02-07 11:20:03 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <App />
+  </div>
+);
+
+//CHALLENGE: Make this app work by applying what you've learnt.
+//1. When new text is written into the input, its state should be saved.
+//2. When the add button is pressed, the current data in the input should be
+//added to an array.
+//3. The <ul> should display all the array items as <li>s
